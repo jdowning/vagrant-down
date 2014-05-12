082af9c5da4e703943076c5c178aaf8273c149a9
Justin Downing <justin@downing.us>
2014-05-12 15:47:23 -0400

    release 0.0.3: update execute block to match upstream

--- lib/vagrant-down/plugin.rb
+++ lib/vagrant-down/plugin.rb
@@ -14,19 +14,19 @@ module VagrantPlugins
 
     end
 
-    class Command < Vagrant.plugin(2, :command)
-
+    class Command < Vagrant.plugin("2", :command)
       def self.synopsis
         'alias for destroy command'
       end
 
       def execute
-        options = {
-          force: false
-        }
+        options = {}
+        options[:force] = false
 
         opts = OptionParser.new do |o|
-          o.banner = "Usage: vagrant down [vm-name]"
+          o.banner = "Usage: vagrant down [options] [name]"
+          o.separator ""
+          o.separator "Options:"
           o.separator ""
 
           o.on("-f", "--force", "Destroy without confirmation.") do |f|
@@ -39,17 +39,25 @@ module VagrantPlugins
         return if !argv
 
         @logger.debug("'Destroy' each target VM...")
-        declined = false
+        declined = 0
+        total    = 0
         with_target_vms(argv, :reverse => true) do |vm|
           action_env = vm.action(
             :destroy, :force_confirm_destroy => options[:force])
 
-          declined = true if action_env.has_key?(:force_confirm_destroy_result) &&
+          total    += 1
+          declined += 1 if action_env.has_key?(:force_confirm_destroy_result) &&
             action_env[:force_confirm_destroy_result] == false
         end
 
-        # Success if no confirms were declined
-        declined ? 1 : 0
+        # Nothing was declined
+        return 0 if declined == 0
+
+        # Everything was declined
+        return 1 if declined == total
+
+        # Some was declined
+        return 2
       end
     end
   end
